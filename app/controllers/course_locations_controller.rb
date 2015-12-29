c223d4b534455d59916155b391114c92dd6ab3e4
Jackie Tremblay <jackietremblayresponsibleadult@gmail.com>
2015-12-29 13:23:18 -0500

    entered links to course_locations

--- app/controllers/course_locations_controller.rb
+++ app/controllers/course_locations_controller.rb
@@ -1,10 +1,14 @@
 class CourseLocationsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :set_course_location, only: [:show, :edit, :update, :destroy]
+  # before_action :authenticate_user!
 
 def index
   @course_locations = CourseLocation.all
 end
 
+def show
+  
+end
 private 
   def course_location_params
     params.require(:course_location).permit(
